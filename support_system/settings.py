f33a62f449be72f2379a65fd05d0f929d0b36325
manju <manjumathu91@gmail.com>
2026-07-24 01:11:03 +0530

    fix static files

--- support_system/settings.py
+++ support_system/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 STATICFILES_DIRS = [
@@ -136,7 +137,7 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 
 
